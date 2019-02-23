391350c803eedfca72d7cfe00b9b2a4b1bedb139
Jonathan Slate <jonathan@slatefamily.net>
2019-02-23 15:06:32 -0500

    adding react app

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{
+    id: 1,
+    username: "Xsamsepi0l"
+  }, {
+    id: 2,
+    username: "XD0loresH4ze"
+  }]);
 });
 
 module.exports = router;
