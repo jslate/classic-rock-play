34eadc3b1723c5376bdae444a27a15268eaa73b2
Jonathan Slate <jonathan@slatefamily.net>
2019-02-24 11:07:13 -0500

    gradient circles, elipsified text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,18 +25,24 @@ class App extends Component {
       );
     }
   }
+
+  elipsify = (string, maxLength) => (
+    string.length > maxLength ? `${string.slice(0,maxLength)}…` : string
+  )
+
   renderSongCirles = () => {
     if (typeof(this.state.songs.map) == 'function') {
       return this.state.songs.map((song, index) => {
-       const y = (index % 8) * 20 + 20;
-       const color = song.artist == 'Led Zeppelin' ? 'red' : 'blue';
-        return (
-          <g key={song.id}>
-            <circle cx={(song.release_year - 1965) * 10} cy={y} r={song.play_count/6} fill={color} fillOpacity="0.5" />
-            <text x={(song.release_year - 1965) * 10} y={y} textAnchor="middle" fontSize="4">{song.name}</text>
-            <text x={(song.release_year - 1965) * 10} y={y + 5} textAnchor="middle" fontSize="4">{song.release_year}</text>
-          </g>
-        )
+       const y = (index % 6) * 20 + 30;
+       const color = song.artist == 'Led Zeppelin' ? 'red' : 'yellow';
+       const fontSize = 300/(200 - song.play_count);
+       return (
+         <g key={song.id}>
+           <circle cx={(song.release_year - 1965) * 10} cy={y} r={song.play_count/6} className={color} fillOpacity="0.6" />
+           <text x={(song.release_year - 1965) * 10} y={y} textAnchor="middle" fontSize={fontSize} fill="black">{this.elipsify(song.name, 20)}</text>
+           <text x={(song.release_year - 1965) * 10} y={y + fontSize * 1.5} textAnchor="middle" fontSize={fontSize} fill="black">{song.release_year}</text>
+         </g>
+       )
       });
     }
   }
@@ -46,6 +52,20 @@ class App extends Component {
       <div className="App">
         <h1>Songs</h1>
         <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="yellowGradient">
+              <stop offset="5%" stop-color="#f7da74" />
+              <stop offset="95%" stop-color="#af8c0e" />
+            </linearGradient>
+            <linearGradient id="redGradient">
+              <stop offset="5%" stop-color="#F00" />
+              <stop offset="95%" stop-color="#F77" />
+            </linearGradient>
+            <linearGradient id="blueGradient">
+              <stop offset="5%" stop-color="#00F" />
+              <stop offset="95%" stop-color="#66F" />
+            </linearGradient>
+          </defs>
           {this.renderSongCirles()}
         </svg>
         <table>
